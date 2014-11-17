8f47d6ae405a5559d12d4521b5705767e8272cca
Simon Prickett <simon@crudworks.org>
2014-11-16 22:27:17 -0500

    Updated action names to correct method names.

--- www/CordovaBoilerPlatePlugin.js
+++ www/CordovaBoilerPlatePlugin.js
@@ -4,20 +4,20 @@ var exec = require('cordova/exec');
 var cordovaBoilerPlatePlugin = {
 	doSomethingNoArgs: function(onSuccess, onError) {
 		console.log('Hello from doSomethingNoArgs');
-		cordova.exec(onSuccess, onError, 'CordovaBoilerPlatePlugin', 'action', []);
+		cordova.exec(onSuccess, onError, 'CordovaBoilerPlatePlugin', 'doSomethingNoArgs', []);
 	},
 
 	doSomethingOneArg: function(arg, onSuccess, onError) {
 		console.log('Hello from doSomethingOneArg');
 		console.log('Called with arg = "' + arg + '"');
-		cordova.exec(onSuccess, onError, 'CordovaBoilerPlatePlugin', 'action', [arg]);
+		cordova.exec(onSuccess, onError, 'CordovaBoilerPlatePlugin', 'doSomethingOneArg', [arg]);
 	},
 
 	doSomethingMultipleArgs: function(argA, argB, onSuccess, onError) {
 		console.log('Hello from doSomethingMultipleArgs');
 		console.log('Called with argA = "' + argA + '"');
 		console.log('Called with argB = "' + argB + '"');
-		cordova.exec(onSuccess, onError, 'CordovaBoilerPlatePlugin', 'action', [argA, argB]);
+		cordova.exec(onSuccess, onError, 'CordovaBoilerPlatePlugin', 'doSomethingMultipleArgs', [argA, argB]);
 	}
 };
 
