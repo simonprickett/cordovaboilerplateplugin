e99f44b21b352b1f4e6cffd75cd805eea7ec3e51
Simon Prickett <simon@crudworks.org>
2014-11-17 13:14:05 -0500

    Added some more boilerplate code for iOS.

--- src/ios/CordovaBoilerPlatePlugin.m
+++ src/ios/CordovaBoilerPlatePlugin.m
@@ -2,9 +2,32 @@
 #import "CordovaBoilerPlatePlugin.h"
 
 @implementation CordovaBoilerPlatePlugin
- - (void)TODOmethodName:(CDVInvokedUrlCommand*)command
+ 
+- (void)doSomethingNoArgs:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
  	// TODO 
+ 	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
+ - (void)doSomethingOneArg:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* argA = [command.arguments objectAtIndex:0];
+
+ 	// TODO 
+ 	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+ - (void)doSomethingMultipleArgs:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+	NSString* argA = [command.arguments objectAtIndex:0];
+    // TODO argB an object?
+
+ 	// TODO 
+
+ 	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
 @end
